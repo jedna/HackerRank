06bb0f2fddbeff5ee289cf5fbf86af37a2a25f3e
Jan Hrubeš <jedna@users.noreply.github.com>
2017-05-09 10:47:48 +0200

    Strings: Making anagrams

---
+++ strings-making-anagrams.java
@@ -0,0 +1,68 @@
+/*
+Alice is taking a cryptography class and finding anagrams to be very useful. We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+Alice decides on an encryption scheme involving two large strings where encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Can you help her find this number?
+
+Given two strings,  and , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+
+This challenge is also available in the following translations:
+
+Chinese
+Russian
+Input Format
+
+The first line contains a single string, . 
+The second line contains a single string, .
+
+Constraints
+
+It is guaranteed that  and  consist of lowercase English alphabetic letters (i.e.,  through ).
+Output Format
+
+Print a single integer denoting the number of characters you must delete to make the two strings anagrams of each other.
+
+Sample Input
+
+cde
+abc
+
+Sample Output
+
+4
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+    public static int numberNeeded(String first, String second) {
+        char[] firstArr = first.toCharArray();
+        char[] secondArr = second.toCharArray();
+        
+        int[] alphabet = new int['z' - 'a' + 1];
+        
+        for(char c : firstArr) {
+            alphabet[c - 'a']++;
+        }
+        
+        for(char c : secondArr) {
+            alphabet[c - 'a']--;
+        }
+        
+        int sum = 0;
+        for(int d : alphabet) {
+            sum += d < 0 ? -d : d;
+        }
+        
+        return sum;
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+}
